7c91eaafdf687a491b23f2ea148b717f080e0c71
Ahmed Ibrahim <42524100+Ahmed-Elesnawy@users.noreply.github.com>
2025-06-26 14:52:38 +0300

    Add Nest config & base repository (#3)
    Co-authored-by: AhmedElesanwy <124180790+AhmedElesanwy@users.noreply.github.com>

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor(private configService: ConfigService) {
+    super({
+      log: configService.get('NODE_ENV') === 'development' ? ['query', 'error', 'warn'] : ['error'],
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
