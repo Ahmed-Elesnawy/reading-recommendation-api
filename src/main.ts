1e1ba90eb3c52ee6d304ffe9c0a41878721d2aca
AhmedElesanwy <124180790+AhmedElesanwy@users.noreply.github.com>
2025-06-27 18:07:02 +0300

    Add cors & rate limitting

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
+import * as csurf from 'csurf';
+import * as cookieParser from 'cookie-parser';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -29,6 +32,15 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api-docs', app, document);
+
+
+  // No need with jwt auth
+  // app.use(cookieParser())
+  // app.use(csurf({ cookie: true }));
+
+  app.enableCors({
+    origin: '*', // TODO: change to the frontend url
+  });
 
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 3000;
